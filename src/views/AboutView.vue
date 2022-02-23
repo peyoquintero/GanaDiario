8274ebd02232e3716ca80ae40f6815a2fd7a827c
rbquintero <raquelbquintero@gmail.com>
2022-02-23 08:10:27 -0500

    Initial code based on vue cli gen

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<script>
+import DemoGrid from './Grid.vue'
+import axios from 'axios';
+
+export default {
+  components: {
+    DemoGrid
+  },
+  data() {return {
+    searchQuery: '',
+    gridColumns: ['Fecha','Codigo','Peso','Sexo','Marca','Lote'],
+    gridData: [],
+  }},
+  mounted() {
+        console.log(`the component is now mounted.`)
+        var url = "https://opensheet.elk.sh/1ZfXM4qnajw8QSaxrx6aXKa_xbMDZe3ryWt8E3alSyEE/4";
+        axios.get(url).then(response => this.gridData = response.data);
+  }
+}
+</script>
+
+<template>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid
+    :data="gridData"
+    :columns="gridColumns"
+    :filter-key="searchQuery">
+  </DemoGrid>
+</template>
